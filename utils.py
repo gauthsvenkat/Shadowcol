fd60352b91b47895db56500574ab1c819393d5bd
Andohuman <gauthsvenkat@yahoo.com>
2019-08-04 22:28:53 +0530

    Created siamese model

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 import librosa
 import numpy as numpy
+import torch
 from torch.utils.data import Dataset
+import torch.nn as nn
+import torch.nn.functional as F
 from glob import glob
 import os
 import random ; random.seed(69)
@@ -31,13 +34,57 @@ class Pairloader(Dataset):
 		assert audio1_mfcc.shape == audio2_mfcc.shape, "MFCC shapes are not the same !" #make sure you're sane
 
 		if self.split == 'train':
-			return [audio1_mfcc, audio2_mfcc], 1 if audio_file1.split('_')[0] == audio_file2.split('_')[0] else 0 #return both mfccs and labels if split is train
+			return [audio1_mfcc[None], audio2_mfcc[None]], 1 if audio_file1.split('_')[0] == audio_file2.split('_')[0] else 0 #return both mfccs and labels if split is train
 		elif self.split == 'valid':
-			return [audio1_mfcc, audio2_mfcc] #else return only mfccs
+			return [audio1_mfcc[None], audio2_mfcc[None]] #else return only mfccs
+
+
+class SiameseNet(nn.Module):
+
+	def __init__(self):
+		super(SiameseNet, self).__init__()
+
+		self.maxpool = nn.MaxPool2d(2)
+
+		self.conv1 = nn.Conv2d(1, 64, 4)
+		self.conv2 = nn.Conv2d(64, 128, 3)
+		self.conv3 = nn.Conv2d(128, 128, 2)
+		
+		self.linear1 = nn.Linear(3584, 1024)
+		self.linear2 = nn.Linear(1024, 1)
+
+	def forward(self, data):
+		res = []
+
+		for i in [0,1]:
+			x = data[i]
+
+			x = self.conv1(x)
+			x = F.relu(x)
+
+			x = self.conv2(x)
+			x = F.relu(x)
+
+			x = self.conv3(x)
+			x = F.relu(x)
+			x = self.maxpool(x)
+
+			x = x.view(x.shape[0], -1)
+
+			x = self.linear1(x)
+			x = F.relu(x)
+
+			res.append(x)
+
+		res = torch.abs(res[1] - res[0])
+		res = self.linear2(res)
+		res = torch.sigmoid(res)
+
+		return res
 
 
 
-
+	
 
 
 
